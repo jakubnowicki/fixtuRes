39d2cf5c45f7e059f67da3a3f29d15be428c04b3
Jakub Nowicki <kuba@appsilon.com>
2020-08-15 10:58:05 +0200

    Random string function.

--- R/basic_types.R
+++ R/basic_types.R
@@ -10,3 +10,15 @@
 integer <- function(min = 0, max = 999999) {
   sample(min:max, 1)
 }
+
+#' Generate random string
+#'
+#' @param length integer string length
+#' @param pattern string pattern for string to follow
+#' @export
+#' @import stringi
+#'
+#' @examples
+string <- function(length, pattern = "[A-Za-z0-9]") {
+  stringi::stri_rand_strings(1, length = length, pattern = pattern)
+}
--- tests/testthat/test-basic_types.R
+++ tests/testthat/test-basic_types.R
@@ -1,3 +1,4 @@
+library(testthat)
 library(checkmate)
 
 context("basic_types")
@@ -30,4 +31,39 @@ test_that("integer function returns an integer higher or equal given minimum", {
 
   # Then
   expect_true(result >= minimum)
+})
+
+test_that("string function returns a string", {
+  # Given
+  length <- fixtuRes::integer(min = 1, max = 10)
+
+  # When
+  result <- string(length)
+
+  # Then
+  expect_string(result)
+})
+
+test_that("string function resturns a string with desired length", {
+   # Given
+  length <- fixtuRes::integer(min = 1, max = 10)
+
+  # When
+  result <- string(length)
+
+  # Then
+  expect_true(nchar(result) == length)
+})
+
+test_that("string function returns a string that follows pattern", {
+  # Given
+  length <- fixtuRes::integer(min = 1, max = 10)
+  letter <- sample(letters, 1)
+  pattern <- glue::glue("[{letter}]")
+
+  # When
+  result <- string(length = length, pattern = pattern)
+
+  # Then
+  expect_string(result, pattern = letter)
 })
