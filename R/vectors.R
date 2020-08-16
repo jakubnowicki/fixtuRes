7735da55bb91643f99bd6ffb658b3a086770211d
Jakub Nowicki <kuba@appsilon.com>
2020-08-16 11:59:35 +0200

    Add functions for vectors from set.

--- R/vectors.R
+++ R/vectors.R
@@ -1,7 +1,21 @@
+# Create a vector of unique values
+replicate_unique <- function(size, generator, args) {
+  output <- NULL
+  while (length(output) < size) {
+    new_value <- do.call(generator, args)
+    if (!(new_value %in% output)) {
+      output <- c(output, new_value)
+    }
+  }
+
+  return(output)
+}
+
 #' Generate random vector of desired type
 #'
 #' @param size integer vector length
 #' @param type "integer", "string", "boolean" or "numeric" type of vector values
+#' @param unique boolean should the output contain only unique values
 #' @param ... arguments passed to function responsible for generating values.
 #'  Check \code{random_integer}, \code{random_string}, \code{random_boolean} and
 #'  \code{random_numeric} for details
@@ -14,7 +28,7 @@
 #' random_vector(10, "numeric", min = 1.5, max = 5)
 #' random_vector(4, "string", length = 4, pattern = "[ACGT]")
 #' random_vector(2, "integer", max = 10)
-random_vector <- function(size, type, ...) {
+random_vector <- function(size, type, unique = FALSE, ...) {
   checkmate::assert_choice(
     type,
     choices = c("integer", "string", "boolean", "numeric")
@@ -33,5 +47,41 @@ random_vector <- function(size, type, ...) {
     )
   }
 
-  replicate(size, do.call(generator, args))
+  if (isTRUE(unique)) {
+    output <- replicate_unique(size, generator, args)
+  } else {
+    output <- replicate(size, do.call(generator, args))
+  }
+
+  return(output)
+}
+
+#' Generate a vector of a values from a set
+#'
+#' @param size integer vector length
+#' @param set vector a set of values to pick from; default: NULL
+#' @param type string if set is NULL generate a random set of type
+#'  ("integer", "string", "boolean", "numeric"); default: NULL
+#' @param set_size integer number of elements in random set; default: NULL
+#' @param ... additional arguments for random set generator.
+#'  For details check \code{random_vector}
+#' @note When using a random set, be aware, that set has to be unique,
+#'  thus if arguments passed to generator do not allow this, the function
+#'  can end up in an infinite loop.
+#' @export
+#'
+#' @examples
+#' set_vector(10, set = c("a", "b", "c"))
+#' set_vector(size = 5, type = "string", set_size = 3)
+set_vector <- function(size, set = NULL, type = NULL, set_size = NULL, ...) {
+  if (is.null(set) && is.null(type)) {
+    stop("Provide following arguments: set or type and set size.")
+  }
+
+  if (is.null(set)) {
+    if (is.null(set_size)) stop("Provide set_size.")
+    set <- random_vector(size = set_size, type = type, unique = TRUE, ...)
+  }
+
+  return(replicate(size, random_from_set(set)))
 }
