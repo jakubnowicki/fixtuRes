f36540d9c27bb06e96d1a7d9852f9092f520d65a
Jakub Nowicki <kuba@appsilon.com>
2020-08-21 16:11:54 +0200

    Update basic types documentation.

--- R/basic_types.R
+++ R/basic_types.R
@@ -1,7 +1,7 @@
 #' Generate random integer
 #'
-#' @param min integer minimum
-#' @param max integer maximum
+#' @param min integer, minimum
+#' @param max integer, maximum
 #' @return random integer
 #' @export
 #'
@@ -13,18 +13,21 @@ random_integer <- function(min = 0, max = 999999) {
 
 #' Generate random string
 #'
-#' @param length integer or NULL (default) string length.
+#' @param length integer or NULL (default), output string length.
 #'  If NULL, length will be random
-#' @param pattern string pattern for string to follow
+#' @param max_length integer, maximum length if length is random.
+#'  Default: 15.
+#' @param pattern string, pattern for string to follow.
+#'  Check \code{\link[stringi]{stringi-search-charclass}} for details.
 #' @return random string
 #' @export
-#' @import stringi
+#' @importFrom stringi stri_rand_strings
 #'
 #' @examples
 #' random_string(length = 5)
-random_string <- function(length = NULL, pattern = "[A-Za-z0-9]") {
+random_string <- function(length = NULL, max_length = 15, pattern = "[A-Za-z0-9]") {
   if (is.null(length)) {
-    length <- random_integer(min = 1, max = 15)
+    length <- random_integer(min = 1, max = max_length)
   }
 
   stringi::stri_rand_strings(1, length = length, pattern = pattern)
@@ -43,8 +46,8 @@ random_boolean <- function() {
 
 #' Generate random numeric
 #'
-#' @param min numeric minimum
-#' @param max numeric maximum
+#' @param min numeric, minimum
+#' @param max numeric, maximum
 #' @return random numeric
 #' @export
 #' @importFrom stats runif
@@ -57,8 +60,8 @@ random_numeric <- function(min = 0, max = 999999) {
 
 #' Choose random element from set
 #'
-#' @param set vector set of values
-#' @return a single element from given set
+#' @param set vector, set of values to choose from
+#' @return a single element from a given set
 #' @export
 #'
 #' @examples
